29a2c2667d97eb2da473a5106a5b89a796275567
bleuapx <123344021+bleuapx@users.noreply.github.com>
2023-03-29 12:18:18 -0500

    Add files via upload

---
+++ PalindromeTester.java
@@ -0,0 +1,10 @@
+//***************************************************************************
+//  PalindromeTester.java
+//
+//  Demonstrates the use of nested while loops.
+//***************************************************************************
+
+import java.util.Scanner;
+
+public class PalindromeTester
+ {
